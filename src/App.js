292b9106023518f60541e430beaa6c3e5816e460
JesusMacias <susomacias@hotmail.com>
2021-11-04 23:37:29 +0100

    configuraciones, router y redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navigation from './layouts/Navigation';
-
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navigation from "./layouts/Navigation";
+import Posts from "./pages/Posts";
+import SignIn from "./pages/SignIn";
+import store from "./store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <div >
-     <Navigation></Navigation>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Navigation></Navigation>
+        </div>
+        <Container>
+          <Routes>
+            <Route path="/" element={<Posts />} />
+            <Route path="/signin" element={<SignIn />} />
+          </Routes>
+        </Container>
+      </Router>
+    </Provider>
   );
 }
 
--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
+import {NavLink} from "react-router-dom";
 
 export default function Navigation() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">React-Java</Navbar.Brand>
+        <Navbar.Brand as={NavLink} to={"/"}>React-Java</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#features">Create Post</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link href="#deets">Crear Cuenta</Nav.Link>
+            <Nav.Link as={NavLink} to={"/signin"}>Crear Cuenta</Nav.Link>
+            <Nav.Link as={NavLink} to={"/"}>Iniciar Sesión</Nav.Link>
 
             <NavDropdown title="Suso" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Posts</NavDropdown.Item>
